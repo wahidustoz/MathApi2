81dfbc17cd0060f5c38ba19dd4c1f323773fc81c
David Wahid <david.wahid@gluwa.com>
2023-03-06 01:05:57 +0900

    test job added :strong:

--- MathApi/Services/MathService.cs
+++ MathApi/Services/MathService.cs
@@ -5,6 +5,6 @@ public class MathService : IMathService
     public Task<long> AddAsync(long a, long b, CancellationToken cancellationToken = default)
     {
         var result = checked(a + b);
-        return Task.FromResult(result)
+        return Task.FromResult(result);
     }
 }
